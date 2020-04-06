464d6f57f609d42de73c5bef91cea57d667492f1
eNobreg <ezra.nobrega@outlook.com>
2020-04-06 17:38:31 -0400

    Fixed pep8 issues

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,7 +8,6 @@ from os.path import exists
 env.hosts = ['35.227.77.93', '35.175.171.90']
 
 
-
 def do_pack():
     """
     Pack file into a .tgz archive
@@ -21,30 +20,29 @@ def do_pack():
         return(filename)
     else:
         return(None)
-    
+
+
 def do_deploy(archive_path):
     """
     Deployment!
     """
     if exists(archive_path) is False:
         return False
-    
+
     file_name = archive_path.split('/')[1]
     path = "/data/web_static/releases"
     try:
         put(archive_path, '/tmp/')
         run("mkdir -p {}{}".format(path, file_name[:-4]))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, file_name[:-4]))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name,
+                                               path, file_name[:-4]))
         run("rm /tmp/{}".format(file_name))
-        run("mv {}{}/web_static/* {}{}/".format(path, file_name[:-4], path, file_name[:-4]))
+        run("mv {}{}/web_static/* {}{}/".format(path, file_name[:-4],
+                                                path, file_name[:-4]))
         run("rm -rf {}{}/web_static".format(path, file_name[:-4]))
         run("rm -rf /data/web_static/current")
-        run("ln -s {}{}/ /data/web_static/current".format(path, file_name[:-4]))
+        run("ln -s {}{}/ /data/web_static/current".format(path,
+                                                          file_name[:-4]))
         return True
     except:
         return False
-    
-    
-        
-    
-    
