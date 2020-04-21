87c54fe52ac37c393cffe3d553bf47272238af2a
eNobreg <ezra.nobrega@outlook.com>
2020-04-21 15:22:20 -0400

    [Fix] Pep8 check

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -89,7 +89,7 @@ class DBStorage():
         session_factory = sessionmaker(self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
-    
+
     def close(self):
         """
         Close
