ed2a35d43d8e337a73aeb166c07da477a3ebc5ab
eNobreg <ezra.nobrega@outlook.com>
2020-04-21 15:16:19 -0400

    [New] Initial push

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''
+Docstring
+'''
+if __name__ == "__main__":
+    from flask import Flask, escape, request
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello(strict_slashes=False):
+        return("Hello HBNB!")
+
+    @app.route('/hbnb')
+    def hbnb(strict_slashes=False):
+        return("HBNB")
+
+    @app.route("/c/<text>")
+    def text_func(text, strict_slashes=False):
+        text = text.replace("_", " ")
+        return("C {content}".format(content=text))
+    app.run(host='0.0.0.0', port='5000')
