ccc1b00f295e0722dc3607696695397f96bb4bbe
eNobreg <ezra.nobrega@outlook.com>
2020-04-21 15:13:59 -0400

    [New] Initial push

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+Docstring
+'''
+if __name__ == "__main__":
+    from flask import Flask, escape, request
+
+    app = Flask(__name__)
+
+    @app.route('/')
+    def hello(strict_slashes=False):
+        return("Hello HBNB!")
+    app.run(host='0.0.0.0', port='5000')
