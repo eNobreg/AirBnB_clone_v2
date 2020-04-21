2937024e54dddf5c30c9317c191a142e06b29e1f
eNobreg <ezra.nobrega@outlook.com>
2020-04-21 15:20:33 -0400

    [New] Initial push

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+A small web application to print states
+'''
+from models import storage
+from models.state import State
+from flask import Flask, request, escape, render_template, abort
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exceptions):
+    storage.close()
+
+
+@app.route("/states_list")
+def list_states(static_slashes=False):
+    return render_template("7-states_list.html", states=storage.all(State))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
